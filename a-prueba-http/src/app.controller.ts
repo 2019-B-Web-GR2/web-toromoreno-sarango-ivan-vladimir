b97b009cb4a16c31fcea5a4e5449991fcb52e752
JosselynBrusil <josselyn.brusil@epn.edu.ec>
2019-12-05 14:15:41 -0500

    Clase

--- a-prueba-http/src/app.controller.ts
+++ a-prueba-http/src/app.controller.ts
@@ -1,29 +1,85 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Head, Headers, HttpCode, InternalServerErrorException, Param, Post, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+// @ts-ignore
+@Controller('pepito') // segemento url -> "/"
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
+  constructor(private readonly appService: AppService) {
+  } // http://localhost:4000/pepito/ GET
+  @Get('hola-mundo') // -> url "hola mundo"
   getHello(): string {
     return this.appService.getHello();
   }
+
+  // http://localhost:4000/pepito/ POST
+  @HttpCode(200)
+  @Post()
+  adiosMundo(): string {
+    const segundos = this.obtenerSegundos();
+    if (segundos % 2 === 0) {
+      return 'Adios mundo!';
+    } else {
+      throw new InternalServerErrorException('Es impar');
+    }
+  }
+
+  private obtenerSegundos(): number {
+    return new Date().getSeconds();
+  }
+
+  @Get('bienvenida')
+  public bienvenida(
+      @Query() parametrosDeConsulta: ObjetoBienvenida,
+      @Query('nombre') nombreUsuario: string,
+      @Query('numero') numeroUsuario: string,
+      @Query('casado') casadoUsuario: string,
+  ): string {
+    console.log(parametrosDeConsulta);
+    return `Mensaje ${parametrosDeConsulta.nombre} Numero: ${parametrosDeConsulta.numero}`;
+  }
+
+  @Get('inscripcion-curso/:idCurso/:cedula')
+  public inscripcionCurso(
+      @Param() parametrosDeRuta: ObjetoInscripcion,
+      @Param('idCurso') idCurso: string,
+      @Param('cedula') cedula: string,
+  ): string {
+    console.log(parametrosDeRuta);
+    return `Te inscribiste al curso: ${idCurso} <br> ${cedula}`;
+  }
+
+  @Post('almorzar')
+  @HttpCode(200)
+  public almorzar(
+      @Body() parametrosDeCuerpo: ObjetoAlmorzar,
+  ): string {
+    console.log(parametrosDeCuerpo);
+    return `Tienes: ${parametrosDeCuerpo.hambre} hambre y ${parametrosDeCuerpo.dinero} dinero`;
+  }
+
+  @Get('obtener-cabeceras')
+  obtenerCabeceras(
+      @Headers() cabeceras,
+      @Headers('numerouno') numeroUno,
+  ) {
+    console.log(cabeceras);
+    return `Las cabeceras son: ${numeroUno}`;
+  }
 }
 
-
-
-
-//Typescript
-
-var nombre: string = "Adrian"; //No recomendable utilizar
-let apellido = "Adrian"; //Mutable
-const cedula = "1718..."; //Inmutable
-
-const casado: boolean = false;
-const casado: number = 5;
-const casado: number = 3.13;
-
-if(casado === false) {
-  
+interface ObjetoBienvenida {
+  nombre?: string;
+  numero?: string;
+  casado?: string;
 }
+
+interface ObjetoInscripcion {
+  idCurso: string;
+  cedula: string;
+}
+
+interface ObjetoAlmorzar {
+  hambre: string;
+  dinero: string;
+}
+
--- a-prueba-http/src/app.module.ts
+++ a-prueba-http/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {UsuarioEntity} from './usuario/usuario.entity';
 
 @Module({
-  imports: [],
+  imports: [
+      TypeOrmModule.forRoot({
+        type: 'mysql',
+        host: 'localhost',
+        port: 32821,
+        username: 'root',
+        password: '1234',
+        database: 'web',
+        entities: [
+            UsuarioEntity,
+        ],
+        synchronize: true, //Con esto se crea en la base de datos
+      }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
